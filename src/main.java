52771431cce3d554b621cce7b93025297b908fd4
TNTKanone1 <oke.koenker@gmail.com>
2025-02-03 12:15:57 +0100

    Addet a method to login to a player account.

--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
+import java.sql.SQLException;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello");
         System.out.println("hello my namsssdae is ");
+
+
+        DB db = new DB();
+        System.out.println(db.getUser_ID("test", "123"));
     }
 
     public void Mathew(){
