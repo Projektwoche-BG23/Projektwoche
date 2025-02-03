b43597f45337fffeee7fcc77badd8b6ba7dc128c
Fynn.Niederbröker <fynn.niederbroeker@web.de>
2025-02-03 11:32:57 +0100

    Character Grundlegen

---
+++ src/Character.java
@@ -0,0 +1,36 @@
+public class Character
+{
+    private int health;
+    private int attack;
+    private int specialAttack;
+    private int defense;
+    private int specialDefense;
+    private int agility;
+
+    private int getAttack()
+    {
+        return attack;
+    }
+
+    private int getHealth()
+    {
+        return health;
+    }
+
+    private int getSpecialAttack()
+    {
+        return specialAttack;
+    }
+    private int getDefense()
+    {
+        return defense;
+    }
+    private int getSpecialDefense()
+    {
+        return specialDefense;
+    }
+    private int getAgility()
+    {
+        return agility;
+    }
+}
