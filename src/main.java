23dae5743ad9e9b569dead85840f98bdc0b1b42c
enes <enestunjj@gmail.com>
2025-02-03 11:53:16 +0100

    testhenrik gelöscht und out gelöscht

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
     static RNG rng = new RNG();
     public static void main(String[] args)
     {
-        rng.randomDrop("chest1");
+        RNG.randomDrop("chest1");
         System.out.println("Hello");
         System.out.println("hello my namsssdae is ");
         Game game = new Game();
