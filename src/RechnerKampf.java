789d04feb49eca1239916aab0dc5b7373e82958e
mocri <mocridhe79@gmail.com>
2025-02-04 11:24:03 +0100

    Unnötigen Code aus RechnerKampf entfernt

--- src/RechnerKampf.java
+++ src/RechnerKampf.java
@@ -4,34 +4,28 @@
  */
 public class RechnerKampf {
 
-    private Character character;
-
-    public RechnerKampf(Character character) {
-        this.character = character;
-    }
-
     /**
      * Calculates the damage dealt by a normal attack.
      */
-    private void attack(Character attacker, Character defender) {
-        int dmg = attacker.getAttack() - (defender.getDefense() * 100 / attacker.getAttack());
-    //    defender.setHealth(defender.getHealth() - dmg);
+    private void attack()
+    {
+
     }
 
     /**
      * Calculates the damage dealt by a special attack.
      */
-    private void specialAttack(Character attacker, Character defender) {
-        int dmg = (attacker.getAttack() * 2) - (defender.getDefense() * 100 / attacker.getAttack());
-     //   defender.setHealth(defender.getHealth() - dmg);
+    private void specialAttack()
+    {
+
     }
 
     /**
      * Calculates the damage dealt by a magic attack.
      */
-    private void magicAttack(Character attacker, Character defender) {
-        int dmg = attacker.getAttack() - (defender.getSpecDefense() * 100 / attacker.getAttack());
-      //  defender.setHealth(defender.getHealth() - dmg);
+    private void magicAttack()
+    {
+
     }
 }
 
