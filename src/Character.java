09db6ccb8aeac8db39a50bff110e06dbc0d67958
Awokados <148012242+Awokados@users.noreply.github.com>
2025-02-04 08:05:52 +0100

    Character #18 (#39)
    * Nicht Fertig
    
    * Character Grundlegen
    
    * Character Grundlegen
    
    * Charakter Grundlagen und der rechner für den Kampf
    
    ---------
    
    Co-authored-by: Fynn.Niederbröker <fynn.niederbroeker@web.de>

---
+++ src/Character.java
@@ -0,0 +1,78 @@
+public class Character
+{
+    private int health = 5;
+    private int attack = 5;
+    private int specialAttack = 5;
+    private int defense = 5;
+    private int specialDefense = 5;
+    private int agility = 5;
+
+
+    public int getAttack()
+    {
+        return attack;
+    }
+    public int getHealth()
+    {
+        return health;
+    }
+    public int getSpecialAttack()
+    {
+        return specialAttack;
+    }
+    public int getDefense()
+    {
+        return defense;
+    }
+    public int getSpecialDefense()
+    {
+        return specialDefense;
+    }
+    public int getAgility()
+    {
+        return agility;
+    }
+
+    /**
+     * Setzt den attack Wert des Characters mit Item fest
+     */
+    private void setAttack()
+    {
+
+    }
+    /**
+     * Setzt den Verteidigzngswert  des Characters mit dem Item
+     */
+    private void setDefense()
+    {
+
+    }
+    /**
+     * Setzt den Lebenswert des Characters mit dem Item fest
+     */
+    private void setHealth()
+    {
+
+    }
+    /**
+     * Setzt den spezialangriff des Characters mit dem item fest
+     */
+    private void setSpecialAttack()
+    {
+
+    }
+    /**
+     * Setzt die Spezialverteidigung des Characters fest mit dem Item
+     */
+    private void setSpecialDefense()
+    {
+
+    }
+    /**
+     * Setzt den Agilitätswert des Characters fest mit Item
+     */
+    private  void setAgility()
+    {
+
+    }
+}
