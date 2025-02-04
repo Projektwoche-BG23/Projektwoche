072ead31caf5d02bfac3b9393e41fa1df9f58c74
Awokados <148012242+Awokados@users.noreply.github.com>
2025-02-04 11:10:51 +0100

    Items rng (#61)
    * UwU
    
    * UwU
    
    * UwU
    
    * Delete out/production/Projektwoche directory
    
    * testhenrik gelöscht und out gelöscht
    
    * testhenrik gelöscht und out gelöscht
    
    * testhenrik gelöscht und out gelöscht
    
    * Jetzt wird eine array von Strings (item namen) zürück gegeben statt es in console auszudrucken. Und eine beispiel anwendung in main kommentiert
    
    * Jetzt wird eine array von Strings (item namen) zürück gegeben statt es in console auszudrucken. Und eine beispiel anwendung in main kommentiert
    
    * Title screen handler
    
    ---------
    
    Co-authored-by: enes <enestunjj@gmail.com>
    Co-authored-by: mikolai.lukomski <kupazdzpa56@gmail.com>

--- src/Game.java
+++ src/Game.java
@@ -13,22 +13,11 @@ public class Game {
     TitleScreenHandler tsHandler = new TitleScreenHandler();
     ChoiceHandler choiceHandler = new ChoiceHandler();
 
-    /**
-     * Dies sind die Schriftarten. Nach belieben ändern
-     */
-
     Font titleFont = new Font("Times New Roman", Font.PLAIN, 170);
     Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
     Font startButtonFont = new Font("Times New Roman", Font.PLAIN, 50);
 
-    /**
-     * Dies gibt die Position in der Story ein. Wichtig für ChoiceHandler
-     */
     String position;
-
-    /**
-     * @Game Titelbildschirm des Spieles
-     */
 
     public Game() {
         try {
@@ -36,10 +25,6 @@ public class Game {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        /**
-         * Dies ist der Hauptframe, auf dem alle anderen Frames hinzugefügt werden.
-         */
 
         frame = new JFrame();
         frame.setSize(1600, 900);
@@ -49,40 +34,22 @@ public class Game {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
 
-        /**
-         * Dieses Panel ist für das anzeigen des Titels
-         */
-
         titleNamePanel = new JPanel();
         titleNamePanel.setBounds(0, 100, 1600, 300);
         titleNamePanel.setBackground(new Color(23, 32, 56));
-        frame.add(titleNamePanel);
-
-        /**
-         * Dieses Label ist der Titel
-         */
-
         titleNameLabel = new JLabel("(Spiel Name)", SwingConstants.CENTER);
-        titleNameLabel.setForeground(new Color(222, 158,65));
+        titleNameLabel.setForeground(new Color(222, 158, 65));
         titleNameLabel.setFont(titleFont);
         titleNamePanel.add(titleNameLabel);
-
-        /**
-         * Dies Panel bietet Platz für die Buttons des Titelscreens
-         */
 
         startButtonPanel = new JPanel();
         startButtonPanel.setBounds(300, 450, 1000, 350);
         startButtonPanel.setBackground(new Color(23, 32, 56));
-        startButtonPanel.setLayout(new GridLayout(4,1));
-
-        /**
-         * Dies ist für das Anzeigen des Start-Buttons
-         */
+        startButtonPanel.setLayout(new GridLayout(4, 1));
 
         startButton = new JButton("Start");
         startButton.setBackground(new Color(23, 32, 56));
-        startButton.setForeground(new Color(222, 158,65));
+        startButton.setForeground(new Color(222, 158, 65));
         startButton.setFont(startButtonFont);
         startButton.addActionListener(tsHandler);
         startButton.setFocusPainted(false);
@@ -90,67 +57,46 @@ public class Game {
 
         ladenButton = new JButton("Laden");
         ladenButton.setBackground(new Color(23, 32, 56));
-        ladenButton.setForeground(new Color(222, 158,65));
+        ladenButton.setForeground(new Color(222, 158, 65));
         ladenButton.setFont(startButtonFont);
-        //Action Listener hinzufügen für Funktion
         startButtonPanel.add(ladenButton);
 
         einstellungenButton = new JButton("Einstellungen");
         einstellungenButton.setBackground(new Color(23, 32, 56));
-        einstellungenButton.setForeground(new Color(222, 158,65));
+        einstellungenButton.setForeground(new Color(222, 158, 65));
         einstellungenButton.setFont(startButtonFont);
-        //Action Listener hinzufügen für Funktion
         startButtonPanel.add(einstellungenButton);
 
         verlassenButton = new JButton("Verlassen");
         verlassenButton.setBackground(new Color(23, 32, 56));
-        verlassenButton.setForeground(new Color(222, 158,65));
+        verlassenButton.setForeground(new Color(222, 158, 65));
         verlassenButton.setFont(startButtonFont);
-        //Action Listener hinzufügen für Funktion
         startButtonPanel.add(verlassenButton);
-
 
         frame.add(titleNamePanel);
         frame.add(startButtonPanel);
         frame.setVisible(true);
     }
 
-    /**
-     * @createGameScreen Hauptbildschirm des Spieles, wo der Spieler seine Optionen auswählt
-     */
     public void createGameScreen() {
-        /**
-         * Löscht den Vorherigen Inhalt
-         */
         titleNamePanel.setVisible(false);
         startButtonPanel.setVisible(false);
 
-        /**
-         * Dies Panel beinhaltet das Dialog Feld
-         */
         mainTextPanel = new JPanel();
-        mainTextPanel.setBounds(100,100,1400, 200);
+        mainTextPanel.setBounds(100, 100, 1400, 200);
         mainTextPanel.setBackground(new Color(23, 32, 56));
         frame.add(mainTextPanel);
-
-        /**
-         * Hier wird der Dialog angezeigt
-         */
 
         mainTextArea = new JTextArea();
         mainTextArea.setBounds(0, 0, 1400, 200);
         mainTextArea.setBackground(new Color(23, 32, 56));
-        mainTextArea.setForeground(new Color(222, 158,65));
+        mainTextArea.setForeground(new Color(222, 158, 65));
         mainTextArea.setFont(normalFont);
         mainTextArea.setLineWrap(false);
         mainTextArea.setWrapStyleWord(true);
         mainTextArea.setEditable(false);
         mainTextArea.setOpaque(false);
         mainTextPanel.add(mainTextArea);
-
-        /**
-         * Dies Feld beinhaltet die Buttons
-         */
 
         choiceButtonPanel = new JPanel();
         choiceButtonPanel.setBounds(90, 630, 1400, 200);
@@ -160,9 +106,8 @@ public class Game {
 
         choiceButton1 = new JButton();
         choiceButton1.setBackground(new Color(23, 32, 56));
-        choiceButton1.setForeground(new Color(222, 158,65));
+        choiceButton1.setForeground(new Color(222, 158, 65));
         choiceButton1.setFont(normalFont);
-        choiceButton1.addActionListener(new ChoiceHandler());
         choiceButton1.setFocusPainted(false);
         choiceButton1.setActionCommand("c1");
         choiceButton1.addActionListener(choiceHandler);
@@ -170,9 +115,8 @@ public class Game {
 
         choiceButton2 = new JButton();
         choiceButton2.setBackground(new Color(23, 32, 56));
-        choiceButton2.setForeground(new Color(222, 158,65));
+        choiceButton2.setForeground(new Color(222, 158, 65));
         choiceButton2.setFont(normalFont);
-        choiceButton2.addActionListener(new ChoiceHandler());
         choiceButton2.setFocusPainted(false);
         choiceButton2.setActionCommand("c2");
         choiceButton2.addActionListener(choiceHandler);
@@ -180,9 +124,8 @@ public class Game {
 
         choiceButton3 = new JButton();
         choiceButton3.setBackground(new Color(23, 32, 56));
-        choiceButton3.setForeground(new Color(222, 158,65));
+        choiceButton3.setForeground(new Color(222, 158, 65));
         choiceButton3.setFont(normalFont);
-        choiceButton3.addActionListener(new ChoiceHandler());
         choiceButton3.setFocusPainted(false);
         choiceButton3.setActionCommand("c3");
         choiceButton3.addActionListener(choiceHandler);
@@ -190,17 +133,12 @@ public class Game {
 
         choiceButton4 = new JButton();
         choiceButton4.setBackground(new Color(23, 32, 56));
-        choiceButton4.setForeground(new Color(222, 158,65));
+        choiceButton4.setForeground(new Color(222, 158, 65));
         choiceButton4.setFont(normalFont);
-        choiceButton4.addActionListener(new ChoiceHandler());
         choiceButton4.setFocusPainted(false);
         choiceButton4.setActionCommand("c4");
         choiceButton4.addActionListener(choiceHandler);
         choiceButtonPanel.add(choiceButton4);
-
-        /**
-         * Dies Panel beinaltet die Stats des Spielers
-         */
 
         playerPanel = new JPanel();
         playerPanel.setBounds(100, 15, 600, 50);
@@ -212,26 +150,21 @@ public class Game {
 
     public void startGame() {
         position = "AnfangsSzene";
-        mainTextArea.setText("The year 384 of the (.) cycle in the kingdom of (placeholder). The great royal family Seidler has\n" +
-                "ruled the land for 13 cycles. For years the country has been struggling with the brutal rule of the\n" +
-                "13th King Thomas von Seidler. But there is hope the only person who can save the KING's city\n");
+        mainTextArea.setText("The year 384 of the (.) cycle in the kingdom of (placeholder)...");
         choiceButton1.setText("Weiter");
         choiceButton2.setText("");
         choiceButton3.setText("");
         choiceButton4.setText("");
     }
 
-    public void introScene(){
+    public void introScene() {
         position = "BeispielOrt1";
-        mainTextArea.setText("and the country .... can save is the one true blood heir to the throne (name of princess) but she\n" +
-                "was driven out some time ago by the corrupt powers of the land. (name of princess)s location is\n" +
-                "unknown nd and bring her back to save the country. \n");
+        mainTextArea.setText("and the country .... can save is the one true blood heir...");
         choiceButton1.setText("Weiter");
         choiceButton2.setText("");
         choiceButton3.setText("");
         choiceButton4.setText("");
     }
-
 
     public class TitleScreenHandler implements ActionListener {
         public void actionPerformed(ActionEvent event) {
@@ -243,39 +176,13 @@ public class Game {
         public void actionPerformed(ActionEvent event) {
             String yourChoice = event.getActionCommand();
 
-            /**
-             * Der erste Switch erfasst den Ort/ die Szene in der, der Spieler gerade ist. Für jede Szene gibt
-             * es mehrere Antworten die mit einem weiteren Switch Statement erfasst werden
-             * Pro ausgewählter Option gibt es verschieden Methoden die dann aufgerufen werden.
-             */
-
             switch (position) {
                 case "AnfangsSzene":
-                    switch (yourChoice) {
-                        case "c1":
-                            introScene();
-                            break;
-                        case "c2":
-                            break;
-                        case "c3":
-                            break;
-                        case "c4":
-                            break;
+                    if ("c1".equals(yourChoice)) {
+                        introScene();
                     }
                     break;
-
                 case "BeispielOrt1":
-                    switch (yourChoice) {
-                        case "c1":
-                            break;
-                        case "c2":
-                            break;
-                        case "c3":
-                            break;
-                        case "c4":
-                            break;
-
-                    }
                     break;
             }
         }
