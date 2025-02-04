74494225f4a0a5c9219b56db72b3f5cd752322cf
mocri <mocridhe79@gmail.com>
2025-02-04 13:42:58 +0100

    Angriffs metoden für Character und Gegnder hinzugefügt und und special werte in Maige umbenannt und die werte luck, Strength, Mana und Mana_regen zum character hinzugefügt

--- src/Character.java
+++ src/Character.java
@@ -1,26 +1,27 @@
 public class Character
 {
+    private final int baseHealth = 5;
+    private final int baseAttack = 5;
+    private final int baseMagicAttack = 5;
+    private final int baseDefense = 5;
+    private final int baseMagicDefense = 5;
+    private final int baseAgility = 5;
+    private final int baseLuck = 5;
+    private final int baseStrength= 0;
+    private final int baseMana= 50;
+    private final int baseMana_reg = 5;
+
     private int health = 5;
     private int attack = 5;
-    private int specialAttack = 5;
+    private int magicAttack = 5;
     private int defense = 5;
-    private int specialDefense = 5;
+    private int magicDefense = 5;
     private int agility = 5;
     private int luck = 5;
     private int strength;
     private int mana;
     private int mana_reg;
 
-    private int baseHealth = 5;
-    private int baseAttack = 5;
-    private int baseSpecialAttack = 5;
-    private int baseDefense = 5;
-    private int baseSpecialDefense = 5;
-    private int baseAgility = 5;
-    private int baseLuck = 5;
-    private int baseStrength;
-    private int baseMana;
-    private int baseMana_reg;
 
     //getter methoden
     public int getAttack()
@@ -31,17 +32,17 @@ public class Character
     {
         return health;
     }
-    public int getSpecialAttack()
+    public int getMagicAttack()
     {
-        return specialAttack;
+        return magicAttack;
     }
     public int getDefense()
     {
         return defense;
     }
-    public int getSpecialDefense()
+    public int getMagicDefense()
     {
-        return specialDefense;
+        return magicDefense;
     }
     public int getAgility()
     {
@@ -67,9 +68,9 @@ public class Character
     //Setter Methoden
     public void setHealth(int newHealth) { health = Math.max(newHealth, 0); }
     public void setDefense(int newDefense) { defense = Math.max(newDefense, 0); }
-    public void setSpecDefense(int newSpecDefense) { specialDefense = Math.max(newSpecDefense, 0); }
+    public void setMagicDefense(int newMagicDefense) { magicDefense = Math.max(newMagicDefense, 0); }
     public void setAttack(int newAttack) { attack = Math.max(newAttack, 0); }
-    public void setSpecAttack(int newSpecAttack) { specialAttack = Math.max(newSpecAttack, 0); }
+    public void setMagicAttack(int newMagicAttack) { magicAttack = Math.max(newMagicAttack, 0); }
     public void setAgility(int newAgility) { agility = Math.max(newAgility, 0); }
     public void setLuck(int newLuck) { luck = Math.max(newLuck, 0); }
     public void setStrength(int newStrength) { luck = Math.max(newStrength, 0); }
