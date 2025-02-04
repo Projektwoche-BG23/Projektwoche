177a0771ade6e7f27bbeb42496437dbf34902e8c
Awokados <148012242+Awokados@users.noreply.github.com>
2025-02-04 08:02:01 +0100

    6 datenbank login (#37)
    * Addet a method to login to a player account.
    
    * Addet a method to login to a player account.
    
    ---------
    
    Co-authored-by: TNTKanone1 <oke.koenker@gmail.com>

--- src/main.java
+++ src/main.java
@@ -1,19 +1,7 @@
 import java.sql.SQLException;
 
-public class main {
-
-
+public class Main {
     static RNG rng = new RNG();
-
-
-        /* --Ein Beispiel Code für chest öffnen und item drop!!!
-         Object[] drops = rng.randomDrop("chest1"); // öffnen von chest (man bekommt eine array von gedroppte items
-         for (int i = 0; i < drops.length; i++) //durch gehen von array
-         {
-           System.out.println(drops[i]); //ausdrucken von item names
-         }
-         */
-
 
     public static void main(String[] args) throws SQLException {
         System.out.println("Hello");
@@ -23,9 +11,16 @@ public class main {
         System.out.println(db.getUser_ID("test", "123"));
 
         Game game = new Game();
+
+        // Beispielcode für das Öffnen einer Kiste und das Erhalten von Items
+        Object[] drops = rng.randomDrop("chest1"); 
+        for (Object drop : drops) {
+            System.out.println(drop); // Item-Namen ausgeben
+        }
     }
 
-    public void Mathew() {
+    public void mathew() {
         System.out.println("Gg");
     }
 }
+
