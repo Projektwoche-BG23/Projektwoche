660e99d5dcdf3963f7ca1992691d90f572915ad3
Awokados <148012242+Awokados@users.noreply.github.com>
2025-02-05 12:01:34 +0100

    6 datenbank login (#112)
    * Addet a method to login to a player account.
    
    * Addet a method to login to a player account.
    
    * Outsource Ordner
    
    ---------
    
    Co-authored-by: TNTKanone1 <oke.koenker@gmail.com>

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 
 public class Main {
     static RNG rng = new RNG();
+    static Inventory inv = new Inventory();
 
     public static void main(String[] args) throws SQLException {
         System.out.println("Hello");
