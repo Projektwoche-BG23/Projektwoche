148246bd1927c137d4d266665247ebd17b302cdf
torboross1 <thorerosin@icloud.com>
2025-02-05 11:31:38 +0100

    Game Class: Auswahloptionen umstrukturiert Neuen Ordner "Images" für Bilder hinzugefügt credit Enes

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,7 @@ import java.awt.event.MouseAdapter;
 public class Game {
     JFrame frame;
     JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel, healtbartextpanel, waffentextpanel;
+    JPanel ImagePanel;
     JLabel titleNameLabel, healtbartext, waffentext;
     JButton startButton, ladenButton, einstellungenButton, verlassenButton;
     JButton choiceButton1, choiceButton2, choiceButton3, choiceButton4;
@@ -200,6 +201,14 @@ public class Game {
         waffentext.setVisible(true);
         healtbartext.setVisible(true);
 
+        ImagePanel = new JPanel();
+        ImagePanel.setBounds(500, 300, 1000, 550);
+        frame.add(ImagePanel);
+
+        ImageIcon imageIcon = new ImageIcon("Images/KerkerHintergrund.png");
+        JLabel label = new JLabel(imageIcon);
+        ImagePanel.add(label);
+
         /**
          * Dies Panel beinhaltet das Dialog Feld
          */
@@ -213,7 +222,7 @@ public class Game {
          */
 
         mainTextArea = new JTextArea();
-        mainTextArea.setBounds(0, 0, 1400, 200);
+        mainTextArea.setBounds(0, 0, 1400, 150);
         mainTextArea.setBackground(new Color(23, 32, 56));
         mainTextArea.setForeground(new Color(222, 158,65));
         mainTextArea.setFont(normalFont);
@@ -228,9 +237,9 @@ public class Game {
          */
 
         choiceButtonPanel = new JPanel();
-        choiceButtonPanel.setBounds(90, 630, 1400, 200);
+        choiceButtonPanel.setBounds(100, 300, 300, 550);
         choiceButtonPanel.setBackground(new Color(23, 32, 56));
-        choiceButtonPanel.setLayout(new GridLayout(2, 2, 10, 10));
+        choiceButtonPanel.setLayout(new GridLayout(4, 1, 10, 10));
         frame.add(choiceButtonPanel);
 
         choiceButton1 = new JButton();
