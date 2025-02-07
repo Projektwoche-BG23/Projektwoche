865de58e4a93d7b873541430372a11ef7775b515
Henrik.Neubauer <henrik.neu@freenet.de>
2025-02-07 09:18:20 +0100

    commit damit ich den scheiß updaten kann
    Took 3 minutes

--- src/recievingLootGUI.java
+++ src/recievingLootGUI.java
@@ -4,6 +4,6 @@ public class recievingLootGUI {
     private JPanel recievingLootMainJPanel;
     private JTextArea textArea1;
     private JTextArea textArea2;
-    private JButton button1;
+    private JButton test;
     private JButton button2;
 }
