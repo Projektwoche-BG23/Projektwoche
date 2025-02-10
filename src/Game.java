4cca9addc66004e69332740e028245f839b8bfc2
mocri <mocridhe79@gmail.com>
2025-02-10 10:16:29 +0100

    Mit glück funktioniert das bilder ding mit

--- src/Game.java
+++ src/Game.java
@@ -415,7 +415,7 @@ public class Game {
     public void setFightPictures(int playerID,String enemyType) throws SQLException {
         DB db = new DB();
         Object[] equip = db.getEquipped(playerID);
-        String weaponID = (String) equip[2];
+        String weaponID = (String) equip[3];
         Object[] weaponInfo = db.itemInfo(Integer.parseInt(weaponID));
         String playerImmagePath = (String) weaponInfo[2];
         //Funktion zum Player bild in fight scene einfügen
@@ -452,6 +452,8 @@ public class Game {
                 enemyPicturePath = "Images/Characters/Gegner/Rahmen/KönigGegner.png";
                 break;
         }
+        System.out.println(playerImmagePath);
+        System.out.println(enemyPicturePath);
         changeImagePlayer(playerImmagePath);
         changeImageGegner(enemyPicturePath);
     }
