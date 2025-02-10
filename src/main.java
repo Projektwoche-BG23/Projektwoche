564c4ee458cf5da1b09b5dbde47b17c73a91b7d9
Awokados <148012242+Awokados@users.noreply.github.com>
2025-02-10 08:45:41 +0100

    Music added and a part of translation

--- src/main.java
+++ src/main.java
@@ -29,14 +29,12 @@ public class main {
         LoginGUI loginGUI = new LoginGUI();
         loginGUI.openLoginWindow();
 
-        Sounds msc = new Sounds();
-        msc.playStory();
+
+
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        msc.playBattle();
-
     }
 }
